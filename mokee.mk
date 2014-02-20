783532267005e4cb837b2f90b7f068c245060995
Ryuinferno <ryuinferno.xda@gmail.com>
2014-02-20 02:09:26 +0100

    hlte: MoKee bringup
    Change-Id: Ie5d329adc46735dd78c7261839cddd7f1520b65f

---
+++ mokee.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := mk_hlte
